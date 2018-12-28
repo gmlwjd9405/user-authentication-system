a2039c3e546cbf9cd87eab63727be728025c995e
kwonheejeong <gmlwjd9405@naver.com>
2018-12-29 02:25:08 +0900

    Update helloDB3

--- src/main/java/com/hee/MainApp.java
+++ src/main/java/com/hee/MainApp.java
@@ -1,15 +1,54 @@
 package com.hee;
 
+import com.hee.model.Offer;
 import com.hee.repository.OfferDAO;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
 
 public class MainApp {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
 
+        // test getRowCount
         OfferDAO offerDAO = (OfferDAO) context.getBean("offerDAO");
         System.out.println("The record count is " + offerDAO.getRowCount());
 
+        // test getOffers
+        List<Offer> offerList = offerDAO.getOffers();
+        for (Offer offer: offerList)
+            System.out.println(offer);
+
+
+        Offer offer = new Offer();
+
+        // test insert
+        offer.setName("hee");
+        offer.setEmail("hee@email.com");
+        offer.setText("test text");
+        if (offerDAO.insert(offer))
+            System.out.println("insert success");
+        else
+            System.out.println("insert fail");
+
+        // test update
+        offer = offerDAO.getOffer("hee1");
+        offer.setName("heee1");
+        if (offerDAO.update(offer))
+            System.out.println("update success");
+        else
+            System.out.println("update fail");
+        // test updated data
+        offer = offerDAO.getOffer("heee1");
+        System.out.println(offer);
+
+        // test delete
+        if (offerDAO.delete(offer.getId()))
+            System.out.println("delete success");
+        else
+            System.out.println("delete fail");
+
+        ((ClassPathXmlApplicationContext) context).close();
     }
 }
--- src/main/java/com/hee/repository/OfferDAO.java
+++ src/main/java/com/hee/repository/OfferDAO.java
@@ -1,10 +1,15 @@
 package com.hee.repository;
 
+import com.hee.model.Offer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 @Repository("offerDAO")
 public class OfferDAO {
@@ -18,5 +23,72 @@ public class OfferDAO {
     public int getRowCount() {
         String sqlStatement = "select count(*) from offers";
         return jdbcTemplate.queryForObject(sqlStatement, Integer.class);
+    }
+
+    // query and return a single object
+    public Offer getOffer(String name) {
+        String sqlStatement = "select * from offers where name=?";
+
+        return jdbcTemplate.queryForObject(sqlStatement, new Object[]{name}, new RowMapper<Offer>() {
+
+            public Offer mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Offer offer = new Offer();
+
+                offer.setId(rs.getInt("id"));
+                offer.setName(rs.getString("name"));
+                offer.setEmail(rs.getString("email"));
+                offer.setText(rs.getString("text"));
+
+                return offer;
+            }
+        });
+    }
+
+    // query and return multiple object
+    public List<Offer> getOffers() {
+        String sqlStatement = "select * from offers";
+
+        return jdbcTemplate.query(sqlStatement, new RowMapper<Offer>() {
+
+            // rowNum 만큼 반복 수행
+            public Offer mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Offer offer = new Offer();
+
+                offer.setId(rs.getInt("id"));
+                offer.setName(rs.getString("name"));
+                offer.setEmail(rs.getString("email"));
+                offer.setText(rs.getString("text"));
+
+                return offer;
+            }
+        });
+    }
+
+    public boolean insert(Offer offer) {
+        String name = offer.getName();
+        String email = offer.getEmail();
+        String text = offer.getText();
+
+        String sqlStatement = "insert into offers (name, email, text) value (?, ?, ?)";
+
+        // return the num of update data
+        return jdbcTemplate.update(sqlStatement, new Object[]{name, email, text}) == 1;
+    }
+
+    public boolean update(Offer offer) {
+        int id = offer.getId();
+        String name = offer.getName();
+        String email = offer.getEmail();
+        String text = offer.getText();
+
+        String sqlStatement = "update offers set name=?, email=?, text=? where id=?";
+
+        return jdbcTemplate.update(sqlStatement, new Object[]{name, email, text, id}) == 1;
+    }
+
+    public boolean delete(int id) {
+        String sqlStatement = "delete from offers where id=?";
+
+        return jdbcTemplate.update(sqlStatement, new Object[]{id}) == 1;
     }
 }
