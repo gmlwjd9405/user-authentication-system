c3f4b78640173388385d5a33832b3415f8eeb1e4
kwonheejeong <gmlwjd9405@naver.com>
2018-12-30 01:34:00 +0900

    Update data valid and Add error msg

--- src/main/java/com/hee/controller/OfferController.java
+++ src/main/java/com/hee/controller/OfferController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -35,9 +36,10 @@ public class OfferController {
     }
 
     @PostMapping("/createOffer")
-    String doCreate(Model model, Offer offer, BindingResult result) {
+    String doCreate(Model model, @Valid Offer offer, BindingResult result) {
         if (result.hasErrors()) {
             System.out.println("===Form data does not validated");
+
             List<ObjectError> errors = result.getAllErrors();
             for (ObjectError error : errors) {
                 System.out.println(error.getDefaultMessage());
@@ -45,7 +47,6 @@ public class OfferController {
             return "createOffer";
         }
         offerService.insert(offer);
-
         return "offerCreated";
     }
 
